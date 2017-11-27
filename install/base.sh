21e2c23b1447e0450fc75b79d74dcdb93095edbd
Sergio Mrtin <sergio.ms.91@gmail.com>
2017-11-27 19:48:37 +0000

    FB

--- install/base.sh
+++ install/base.sh
@@ -4,3 +4,5 @@ apt-get update -y && apt-get install -y wget && rm -rf /var/lib/apt/lists/*
 apt-get update -y && apt-get install -y default-jre && rm -rf /var/lib/apt/lists/*
 apt-get update -y && apt-get install -y tar && rm -rf /var/lib/apt/lists/*
 apt-get update -y && apt-get install -y vim && rm -rf /var/lib/apt/lists/*
+apt-get update -y && apt-get install -y locales
+locale-gen en_US.UTF-8

8d74443d1b681186ac272f73c949204319c0d98b
Sergio Mrtin <sergio.ms.91@gmail.com>
2018-01-11 17:56:22 +0000

    FB

--- Start/Ubuntu_MAC/start_master.sh
+++ Start/Ubuntu_MAC/start_master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dir="notebooks"
+dir="$(pwd)/notebooks"
 docker network rm docker4science
 docker network create --subnet 172.27.0.0/24 --driver bridge docker4science
-docker run --net=docker4science -v $pwd/../../$dir:/notebooks/ -ti --rm --name master -p 10088:8888 -p 10080:8080 -p 10087:8787 docker4science:1.0
+docker run --net=docker4science -v $dir:/notebooks/ -ti --rm --name master -p 10088:8888 -p 10080:8080 -p 10087:8787 docker4science:1.0

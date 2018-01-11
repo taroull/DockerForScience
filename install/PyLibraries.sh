63727767e7b2f532411f91541d5285e36d671da8
Sergio Mrtin <sergio.ms.91@gmail.com>
2018-01-11 20:14:51 +0000

    FB

--- install/PyLibraries.sh
+++ install/PyLibraries.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 PIP_TARGET=/notebooks/PyLibraries
 export PIP_TARGET=${PIP_TARGET}
-mkdir -p ~/.pip
 mkdir -p ${PIP_TARGET}/pip
+export PIP_DOWNLOAD_CACHE=${PIP_TARGET}/pip
+mkdir -p ~/.pip
 echo -e "[global]\ntarget=${PIP_TARGET}\ndownload_cache = ${PIP_TARGET}/pip" >> ~/.pip/pip.conf
 echo "export PIP_TARGET=${PIP_TARGET}" >> ~/.bashrc
